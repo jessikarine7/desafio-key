1c715a2ff083af8f1f6db99e95e26459df54f767
jessikarine7 <jessikarine.santos@gmail.com>
2022-03-30 16:48:26 -0300

    configs on heroku
    Signed-off-by: jessikarine7 <jessikarine.santos@gmail.com>

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({static: path.join(__dirname, 'dist')});
 
 server.use(middlewares);
 server.use(express.static('dist'));
