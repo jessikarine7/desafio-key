7bd84122a904226261d177dccca61bfe9e467a60
jessikarine7 <jessikarine.santos@gmail.com>
2022-03-30 16:26:30 -0300

    configs on heroku

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-console.log(express.static(path.join(__dirname, 'dist')));
-express()
-  .use(express.static('dist'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// console.log(express.static(path.join(__dirname, 'dist')));
+// express()
+//   .use(express.static('dist'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+server.use(express.static('dist'));
+server.listen(PORT);
