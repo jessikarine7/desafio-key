b51cfb5c224b24f96ee62caecddc8b740b675479
jessikarine7 <jessikarine.santos@gmail.com>
2022-03-30 14:44:46 -0300

    configurations on heroku
    Signed-off-by: jessikarine7 <jessikarine.santos@gmail.com>

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 console.log(express.static(path.join(__dirname, 'dist')));
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
+  .use(express.static('dist'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
