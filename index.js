a23acacffdc94fa50f593a6edae793c9f70de0ba
jessikarine7 <jessikarine.santos@gmail.com>
2022-03-30 14:32:27 -0300

    fix server
    Signed-off-by: jessikarine7 <jessikarine.santos@gmail.com>

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-
+console.log(express.static(path.join(__dirname, 'dist')));
 express()
   .use(express.static(path.join(__dirname, 'dist')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
